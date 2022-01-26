bbd7465e1b4af5cc67052381c26d44430ae65d61
Pauloszf <paulosergio.zaguini@gmail.com>
2022-01-26 12:22:11 -0400

    Corrigido css do contato

--- src/Pages/Contato.js
+++ src/Pages/Contato.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Button } from 'primereact/button';
 
 import '../css/Contato.css';
 
@@ -13,23 +16,29 @@ export default class Contato extends Component {
             mensagem: '',
         }
     }
+
+    setValue = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
     render() {
         return (
-            <div className="contato">
+            <div className="p-d-flex mt-4 p-align-center p-justify-center">
                 <div className="formulario">
                     <form action="">
                         <label htmlFor="">Nome</label>
-                        <input type="text" />
+                        <InputText className="w-full my-2" name="nome" value={this.state.nome} onChange={(e) => this.setValue(e)} />
                         <label htmlFor="">E-mail</label>
-                        <input type="text" />
+                        <InputText className="w-full my-2" name="email" value={this.state.email} onChange={(e) => this.setValue(e)} />
                         <label htmlFor="">Telefone</label>
-                        <input type="text" />
+                        <InputText className="w-full my-2" name="telefone" value={this.state.telefone} onChange={(e) => this.setValue(e)}/>
                         <label htmlFor="">Mensagem</label>
-                        <textarea name="" id="" cols="30" rows="10" />
+                        <InputTextarea className="w-full my-2" rows={10} cols={30} name="mensagem" value={this.state.mensagem} onChange={(e) => this.setValue(e)} autoResize />
                     </form>
-                    <div className="botoes">
-                        <button className="btEnviar">Enviar</button>
-                        <button className="btLimpar">Apagar</button>
+                    <div className="p-d-flex">
+                        <Button className="w-full my-2 mx-1 btEnviar" label="Enviar" />
+                        <Button className="w-full my-2 mx-1 btLimpar" label="Apagar" />
                     </div>
                 </div>
             </div>
--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import CardConteudo from '../Components/CardConteudo'
+import { Card } from 'primereact/card';
 
 import micFeed from '../Assets/micFeed.png'
 import '../css/Feed.css'
@@ -8,8 +9,8 @@ export default class Feed extends Component {
     render() {
         return (
             <div className="conteudoFeed p-d-flex p-align-center p-justify-center">
-                <div className="postagens">
-                    <div className="divisorPostagens p-d-flex p-align-center p-justify-center">
+                <div className="postagens p-d-none p-d-md-inline">
+                    <div className="divisorPostagens w-full p-d-flex p-align-center p-justify-center">
                         <img src={micFeed} alt="" />
                         <h1>Postagens novas</h1>
                         <img src={micFeed} alt="" />
@@ -39,6 +40,11 @@ export default class Feed extends Component {
                             descricao="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, ducimus. Quas aut aspernatur optio officia. Suscipit at consequuntur odio, quas rerum ratione ea qui ducimus, eaque dolore eos esse accusantium!" 
                         />
                     </div>
+                </div>
+                <div className="feedResponsivo pb-8 p-d-md-none p-d-inline" >
+                    <Card className='w-13rem p-1 h-25rem line-height-2 text-overflow-ellipsis overflow-y-hidden' title="Titulo incrivel 4" header={<img alt="Card" src="https://picsum.photos/200/200" />}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia voluptates voluptatibus rerum expedita officia deserunt, harum impedit minus possimus deleniti rem explicabo quibusdam incidunt corrupti placeat magnam nisi sunt provident.
+                    </Card>
                 </div>
             </div>
         )
